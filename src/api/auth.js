5266d9028ac5e5669c33a444b2189b48c34d202b
vismay1888 <vismaysheelabhadra18@gmail.com>
2025-03-19 12:51:43 +0530

    done

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080";
+const API_URL = "https://ecom-backend-ibws.onrender.com";
 
 export async function login(credentials) {
   const response = await fetch(`${API_URL}/login`, {
--- src/api/product.js
+++ src/api/product.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080";
+const API_URL = "https://ecom-backend-ibws.onrender.com";
 
 export async function deleteProduct(product) {
   const response = await fetch(`${API_URL}/product/delete/${product}`, {
